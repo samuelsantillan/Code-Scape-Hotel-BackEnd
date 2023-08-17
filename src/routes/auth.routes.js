e3d56bfdbf8d6eb0f69acd64ddad926443d6adc5
luki1qq <tu_nuevo_email@example.com>
2023-08-17 19:44:37 -0300

    config de rutas

---
+++ src/routes/auth.routes.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import {
+  login,
+  logout,
+  register,
+  verifyToken,
+  getUsers,
+  updateUser,
+  getUser,
+  deleteUser
+} from "../controllers/auth.controller.js";
+import { validateSchema } from "../middlewares/validator.middleware.js";
+import { loginSchema, registerSchema } from "../schemas/auth.schema.js";
+
+const router = Router();
+
+router.post("/register", validateSchema(registerSchema), register);
+router.post("/login", validateSchema(loginSchema), login);
+router.get("/verify", verifyToken);
+router.post("/logout",  logout);
+router.get("/getUsers", getUsers);
+router.put('/updateUser/:id', updateUser );
+router.get('/getUser/:id', getUser);
+router.delete('/deleteUser/:id', deleteUser);
+export default router;
--- src/routes/room.routes.js
+++ src/routes/room.routes.js
@@ -1,8 +1,22 @@
 import { Router } from "express";
-import { roomCreate } from "../controllers/admin.room.controller.js";
+import {
+  createRoom,
+  deleteRoom,
+  getRoom,
+  updateRoom,
+  getRooms,
+  profile
+} from "../controllers/admin.room.controller.js";
+
+import { auth} from "../middlewares/auth.middleware.js";
+
 
 const router = Router();
 
-router.post("/roomCreate", roomCreate);
-
+router.post("/roomCreate",  createRoom);
+router.delete("/roomDelete/:id",  deleteRoom);
+router.get("/roomGet",  getRooms);
+router.get("/roomGet/:id", getRoom)
+router.put("/roomUpdate/:id", updateRoom);
+router.get("/profile", auth, profile);
 export default router;
