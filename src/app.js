f07b35fc282e90ed0a3d0b37cd3f860870cbc1be
luki1qq <tu_nuevo_email@example.com>
2023-08-17 19:45:10 -0300

    configuracion principal

--- src/app.js
+++ src/app.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import morgan from 'morgan';
-import roomRoutes from './routes/room.routes.js';
-
-
+import express from "express";
+import morgan from "morgan";
+import roomRoutes from "./routes/room.routes.js";
+import cors from "cors";
+import authRoutes from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
 const app = express();
-
-app.use(morgan('dev')); // morgan is a middleware
-app.use(express.json()); // express.json is a middleware
-app.use('/api', roomRoutes);
-
+app.use(
+  cors({
+    origin: ["http://localhost:5173","http://127.0.0.1:5173"],
+    credentials: true,
+  })
+);
+app.use(morgan("dev"));
+app.use(express.json()); // Para que express entienda los datos que vienen en formato json
+app.use(cookieParser());
+app.use("/api", roomRoutes);
+app.use("/api", authRoutes);
 export default app;
