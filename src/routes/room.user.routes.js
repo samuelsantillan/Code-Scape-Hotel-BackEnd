6a71835171c81b69bef13fc413341edec416bb1d
luki1qq <tu_nuevo_email@example.com>
2023-08-22 21:15:14 -0300

    se agrego nuevas rutas de acceso /getAvailableDadtss y /getAvailableDate/:id

--- src/routes/room.user.routes.js
+++ src/routes/room.user.routes.js
@@ -1,7 +1,15 @@
 import { Router } from "express";
 
 import { createRoomUserReservation } from "../controllers/room.user.reservation.controller.js";
+import { getAvailableDates, getAvailableDate} from "../controllers/available.date.controller.js";
+
 
 const router = Router();
 
 router.post("/createRoomUserReservation", createRoomUserReservation);
+
+router.get("/getAvailableDates", getAvailableDates);
+
+router.get('/getAvailableDate/:id', getAvailableDate);
+
+export default router;
