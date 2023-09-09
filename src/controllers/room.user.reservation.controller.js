8456e837bfeb7b46752ec507bf7817a8dedf1189
luki1qq <tu_nuevo_email@example.com>
2023-09-09 16:47:22 -0300

    se creo la funcion para enviar mail

--- src/controllers/room.user.reservation.controller.js
+++ src/controllers/room.user.reservation.controller.js
@@ -1,21 +1,23 @@
-import RoomUserReservation from "../models/room.user.reservation.model.js"; 
-import nodemailer from 'nodemailer';
-const transporter = nodemailer.createTransport({
-  service: 'Gmail',
+import RoomUserReservation from "../models/room.user.reservation.model.js";
+import nodemailer from "nodemailer";
+import { emailTemplate } from "../views/email.js";
+
+export const transporter = nodemailer.createTransport({
+  service: "Gmail",
   auth: {
-      user: 'codescapehotel@gmail.com',
-      pass: 'yuocvgmxujkcqock',
+    user: "codescapehotel@gmail.com",
+    pass: "yuocvgmxujkcqock",
   },
 });
 
 export const createRoomUserReservation = async (req, res) => {
-  const { idRoom, idUser, startDate, endDate } = req.body; 
+  const { idRoom, idUser, startDate, endDate } = req.body;
   console.log(req.body);
   try {
     const newRoomUserReservation = new RoomUserReservation({
       idRoom: idRoom,
       idUser: idUser,
-      startDate: startDate, 
+      startDate: startDate,
       endDate: endDate,
     });
     const roomUserReservationSaved = await newRoomUserReservation.save();
@@ -36,43 +38,41 @@ export const getRoomsUserReservation = async (req, res) => {
   }
 };
 
-
 export const getRoomUserReservation = async (req, res) => {
   const { id } = req.params;
   try {
-    const roomUserReservation = await RoomUserReservation.find({ idRoom : id });
+    const roomUserReservation = await RoomUserReservation.find({ idRoom: id });
     res.json(roomUserReservation);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
 
-export const deleteAllRoomUserReservation =   async (req, res) => {
+export const deleteAllRoomUserReservation = async (req, res) => {
   try {
     const roomUserReservation = await RoomUserReservation.deleteMany();
     res.json(roomUserReservation);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
-}
+};
 
-const sendConfirmationEmail = async (userEmail, reservationDetails) => {
+export const sendConfirmationEmail = async (req, res) => {
+  const { nameHabitation, type, startDate, endDate, email } = req.body;
+  console.log(req.body);
   try {
     const mailOptions = {
       from: "codescapehotel@gmail.com",
-      to: userEmail,
+      to: email,
       subject: "Confirmación de reserva",
-      text: `¡Tu reserva se ha confirmado!\n
-        Detalles de la reserva:\n
-        - Habitación: ${reservationDetails.idRoom}\n
-        - Usuario: ${reservationDetails.idUser}\n
-        - Fecha de inicio: ${reservationDetails.startDate}\n
-        - Fecha de finalización: ${reservationDetails.endDate}\n`,
+      html: emailTemplate(nameHabitation),
     };
 
     await transporter.sendMail(mailOptions);
-    console.log(`Correo electrónico enviado a ${userEmail} con éxito.`);
+    // console.log(`Correo electrónico enviado a ${email} con éxito.`);
+    res.json({ message: `Correo electrónico enviado a ${email} con éxito.` });
+    // res.status(201).json(nameHabitation);
   } catch (error) {
-    console.error("Error al enviar el correo electrónico:", error);
+    res.status(500).json({ message: error.message });
   }
 };
